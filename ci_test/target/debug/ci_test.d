87bf27b741b9bbe22a6fcb8dff0e4cd7bde5ac3c
murakami-td <h.murakami@tcdigital.jp>
2024-07-22 12:18:27 +0000

    test

---
+++ ci_test/target/debug/ci_test.d
@@ -0,0 +1,1 @@
+/workspaces/test20240723-1/ci_test/target/debug/ci_test: /workspaces/test20240723-1/ci_test/src/main.rs
---
+++ ci_test/target/debug/deps/ci_test-73c249dd85ef80eb.d
@@ -0,0 +1,5 @@
+/workspaces/test20240723-1/ci_test/target/debug/deps/ci_test-73c249dd85ef80eb: src/main.rs
+
+/workspaces/test20240723-1/ci_test/target/debug/deps/ci_test-73c249dd85ef80eb.d: src/main.rs
+
+src/main.rs:
